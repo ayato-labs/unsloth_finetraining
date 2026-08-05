419b1e382f4f6c9df5ad2b1d5cdc35598e37b2b4
ayato-labs <ayato-labs@users.noreply.github.com>
2026-08-05 21:11:39 +0900

    feat: ベースモデルとトークナイザーの埋め込みサイズアライン共通ヘルパーを追加

---
+++ src/common/model_utils.py
@@ -0,0 +1,15 @@
+from src.common.logger import logger
+
+
+def align_model_embeddings(model, tokenizer, pad_to_multiple_of: int = 8) -> None:
+    """ベースモデルとトークナイザーの語彙数が一致しない場合、動的にリサイズを実行する共通ヘルパー関数"""
+    target_vocab_size = len(tokenizer)
+    current_vocab_size = model.get_input_embeddings().weight.shape[0]
+
+    if current_vocab_size != target_vocab_size or (current_vocab_size % pad_to_multiple_of != 0):
+        model.resize_token_embeddings(target_vocab_size, pad_to_multiple_of=pad_to_multiple_of)
+        new_vocab_size = model.get_input_embeddings().weight.shape[0]
+        logger.info(
+            f"token_embeddings_aligned: resized from {current_vocab_size} to {new_vocab_size} "
+            f"(target len={target_vocab_size}, pad_multiple={pad_to_multiple_of})"
+        )
